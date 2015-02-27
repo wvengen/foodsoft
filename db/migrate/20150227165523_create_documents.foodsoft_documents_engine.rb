e6c042a9e3ea6fb64ab4d74d4201ee5c2f1a2a8c
wvengen <dev-voko@willem.engen.nl>
2015-02-27 18:00:37 +0100

    Fix migration

---
+++ db/migrate/20150227165523_create_documents.foodsoft_documents_engine.rb
@@ -0,0 +1,15 @@
+# This migration comes from foodsoft_documents_engine (originally 20150202000000)
+class CreateDocuments < ActiveRecord::Migration
+  def self.up
+    create_table :documents do |t|
+      t.string :name
+      t.binary :content, limit: 16.megabyte
+      t.integer :user_id
+      t.datetime :created_at
+    end
+  end
+
+  def self.down
+    drop_table :documents
+  end
+end
